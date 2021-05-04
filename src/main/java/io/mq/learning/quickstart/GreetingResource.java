76a0865502f70cd6c082f322dc91ab3def210c77
MQ <morris.qiao@gmail.com>
2021-05-04 22:37:59 +0200

    first commit CH03

--- src/main/java/io/mq/learning/quickstart/GreetingResource.java
+++ src/main/java/io/mq/learning/quickstart/GreetingResource.java
@@ -1,16 +1,39 @@
 package io.mq.learning.quickstart;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.annotation.PostConstruct;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+//Identifies the URI path of the current resource
 @Path("/hello")
 public class GreetingResource {
 
+    //Responds to HTTP GET requests
     @GET
+    //Defines the media type(s) that are returned
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        //Returns plain text
         return "hello";
     }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void create(String message) {
+        System.out.println("Create");
+    }
+
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String update(String message) {
+        System.out.println("Update");
+        return message;
+    }
+
+    @DELETE
+    public void delete() {
+        System.out.println("Delete");
+    }
+
 }
