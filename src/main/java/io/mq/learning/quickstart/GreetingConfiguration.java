c96f3f4504bc4429b038741ac50da874bdab689c
MQ <morris.qiao@gmail.com>
2021-05-10 23:44:03 +0200

    CH04 validating configuration value

--- src/main/java/io/mq/learning/quickstart/GreetingConfiguration.java
+++ src/main/java/io/mq/learning/quickstart/GreetingConfiguration.java
@@ -2,6 +2,8 @@ package io.mq.learning.quickstart;
 
 import io.quarkus.arc.config.ConfigProperties;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @ConfigProperties(prefix = "greeting")
@@ -9,6 +11,9 @@ public class GreetingConfiguration {
     public String message;
     public String suffix = "!";
     public OutputConfiguration output;
+    @Min(1)
+    @Max(3)
+    public Integer repeat;
 
     public static class OutputConfiguration { public List<String> recipients;
     }
